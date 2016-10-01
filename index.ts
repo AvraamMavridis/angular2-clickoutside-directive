a4cabe6c4c14e6aaea9e6be0468f6dfae8911d11
AvraamMavridis <avraammauridis@gmail.com>
2016-10-01 17:28:32 +0200

    feat: Remove event listeners onDestroy

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
-import { Directive, ElementRef, Output, EventEmitter } from 'angular2/core';
+import { Directive, ElementRef, Output, EventEmitter, OnInit, OnDestroy } from 'angular2/core';
 
 const clickOutsideEvents = [ 'mousedown', 'touchstart' ];
 const isDescendant = ( el, target ) => target !== null ? el === target || isDescendant( el, target.parentNode ) : false;
 
 @Directive({ selector: '[clickOutside]' })
-export class ClickOutsideDirective
+export class ClickOutsideDirective implements OnInit, OnDestroy
 {
-    constructor( public el : ElementRef )
-    {
-       clickOutsideEvents.forEach( e => document.addEventListener( e, this.handleClickOutside.bind( this ) ) )
+    public onClickOutsideHandler : EventListener;
+
+    constructor( public el : ElementRef ){
+      this.onClickOutsideHandler = this.handleClickOutside.bind( this )
+    }
+
+    ngOnInit(){
+      clickOutsideEvents.forEach( e => document.addEventListener( e, this.onClickOutsideHandler ) )
+    }
+
+    ngOnDestroy() {
+      clickOutsideEvents.forEach( e => document.removeEventListener( e, this.onClickOutsideHandler ) );
     }
 
     @Output() onClickOutside : EventEmitter<any> = new EventEmitter();
